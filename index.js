589975b294917f4807e111485821c9b141d4a06a
Dylan L Jones <darkraikiri@gmail.com>
2018-02-01 01:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
-  return `happy ${holiday}, ${name} !`
+  return `happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
